a481371cd8e0b8f7662415baa2edb5a6f9aa1edb
Leonardo <leonardo.ayalao@pucp.edu.pe>
2022-11-18 09:39:50 -0500

    Avance popup

--- app/src/main/java/pe/edu/pucp/lab5/ListarActivity.java
+++ app/src/main/java/pe/edu/pucp/lab5/ListarActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -25,7 +27,32 @@ public class ListarActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NotNull MenuItem item){
         switch(item.getItemId()){
+            case R.id.btn_calendar:
+                View menuItemView = findViewById(R.id.btn_calendar);
+                PopupMenu popupMenu = new PopupMenu(this,menuItemView);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_listar,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch(item.getItemId()){
+                            case R.id.btn_fechaInicio:
 
+                                return true;
+                            case R.id.btn_horaInicio:
+
+                                return true;
+                            case R.id.btn_fechaFinal:
+
+                                return true;
+                            case R.id.btn_horaFinal:
+
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
