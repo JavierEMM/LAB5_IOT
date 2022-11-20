208a93926f67f0fda6f5a0bee8d72580822263af
Diego Vargas <a20191027@pucp.edu.pe>
2022-11-19 19:50:57 -0500

    Recycler View

--- app/src/main/java/pe/edu/pucp/lab5/ListarActivity.java
+++ app/src/main/java/pe/edu/pucp/lab5/ListarActivity.java
@@ -1,21 +1,71 @@
 package pe.edu.pucp.lab5;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import pe.edu.pucp.lab5.Entity.Actividad;
+
 public class ListarActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
+    ArrayList<Actividad> listaActividades = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerActividades);
+
+        ListaActividadesAdapter adapter = new ListaActividadesAdapter();
+        adapter.setContext(ListarActivity.this);
+        firebaseDatabase= FirebaseDatabase.getInstance();
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("Hitos");
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot children : snapshot.getChildren() ){
+                    Actividad actividad = children.getValue(Actividad.class);
+                    listaActividades.add(actividad);
+                    adapter.setListaActividades(listaActividades);
+                    recyclerView.setAdapter(adapter);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(ListarActivity.this));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+
+
+
     }
 
     @Override
