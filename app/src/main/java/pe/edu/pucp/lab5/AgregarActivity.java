3b0b1422b16a7b13076b34ba35ac1d944bb36631
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-11-20 02:00:26 -0500

    AgregarActivity casi terminado

--- app/src/main/java/pe/edu/pucp/lab5/AgregarActivity.java
+++ app/src/main/java/pe/edu/pucp/lab5/AgregarActivity.java
@@ -1,18 +1,29 @@
 package pe.edu.pucp.lab5;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.Calendar;
 
@@ -21,6 +32,26 @@ import pe.edu.pucp.lab5.Entity.Actividad;
 public class AgregarActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK){
+                    Uri uri = result.getData().getData();
+                    StorageReference child = storageReference.child("photo.jpg");
+                    child.putFile(uri)
+                            .addOnSuccessListener(taskSnapshot -> Log.d("msg","archivo subido exitosamente"))
+                            .addOnFailureListener(e -> Log.d("msg","error",e.getCause()))
+                            .addOnProgressListener(snapshot -> {
+                                long bytesTransferidos = snapshot.getBytesTransferred();
+                                long bytesTotales = snapshot.getTotalByteCount();
+                                double progreso = (100.0 * bytesTransferidos) / bytesTotales;
+                                Log.d("msg","Porcentaje de subida: " + Math.round(progreso) + "%");
+
+                            });
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +60,7 @@ public class AgregarActivity extends AppCompatActivity {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
+        firebaseStorage = firebaseStorage.getInstance();
 
         EditText editTextTitulo = findViewById(R.id.editText_titulo);
         String tituloStr = editTextTitulo.getText().toString();
@@ -55,6 +87,7 @@ public class AgregarActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+        String fechaInicioStr = editTextFechaInicio.getText().toString();
 
         EditText editTextHoraInicio = findViewById(R.id.editTextHoraInicio);
         Button btnHoraInicio = findViewById(R.id.btnHoraInicio);
@@ -73,6 +106,7 @@ public class AgregarActivity extends AppCompatActivity {
                 timePickerDialog.show();
             }
         });
+        String horaInicioStr = editTextHoraInicio.getText().toString();
 
         EditText editTextFechaFinal = findViewById(R.id.editTextFechaFinal);
         Button btnFinal = findViewById(R.id.btnFechaFinal);
@@ -93,6 +127,7 @@ public class AgregarActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+        String fechaFinalStr = editTextFechaFinal.getText().toString();
 
         EditText editTextHoraFinal = findViewById(R.id.editTextHoraFinal);
         Button btnHoraFinal = findViewById(R.id.btnHoraFinal);
@@ -111,7 +146,48 @@ public class AgregarActivity extends AppCompatActivity {
                 timePickerDialog.show();
             }
         });
+        String horaFinalStr = editTextHoraFinal.getText().toString();
 
-        
+        Button btnimagen = findViewById(R.id.btnImagen);
+        btnimagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.setType("image/jpeg");
+                launcher.launch(intent);
+            }
+        });
+
+        StorageReference photoRef = storageReference.child("photo.jpg");
+        ImageView imageView = findViewById(R.id.imageView);
+        Glide.with(this).load(photoRef).into(imageView);
+
+        Button btnAgregar = findViewById(R.id.btnAgregar);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Actividad actividad = new Actividad();
+                actividad.setTitulo(tituloStr);
+                actividad.setDescripcion(descripcionStr);
+                actividad.setFechaInicio(fechaInicioStr);
+                actividad.setHoraInicio(horaInicioStr);
+                actividad.setFechaFin(fechaFinalStr);
+                actividad.setHoraFin(horaFinalStr);
+                actividad.setFoto("photo.jpg");
+                databaseReference.push().setValue(actividad);
+                Intent intent = new Intent(AgregarActivity.this,ListarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnCancelar = findViewById(R.id.btnCancelar);
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AgregarActivity.this,ListarActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
